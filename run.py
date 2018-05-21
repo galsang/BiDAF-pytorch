6212d045c3e345659c0943499f1758148cb3a4b4
galsang <taeuk@europa.snu.ac.kr>
2018-05-22 02:16:40 +0900

    modified

--- run.py
+++ run.py
@@ -76,6 +76,7 @@ def test(model, args, data):
         batch_loss = criterion(p1, batch.s_idx) + criterion(p2, batch.e_idx)
         loss += batch_loss.item()
 
+    """
         for i in range(len(batch.c_word)):
             # TODO: optimization needed for finding the best span
             s_idx, e_idx = 0, 0
@@ -99,22 +100,23 @@ def test(model, args, data):
 
     results = evaluate.main(args)
     return loss, results['exact_match'], results['f1']
-
+    """
+    return loss, 0, 0
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--char-dim', default=8, type=int)
     parser.add_argument('--char-channel-width', default=5, type=int)
     parser.add_argument('--char-channel-size', default=100, type=int)
-    parser.add_argument('--dev-batch-size', default=100, type=int)
+    parser.add_argument('--dev-batch-size', default=50, type=int)
     parser.add_argument('--dev-file', default='dev-v1.1.json')
     parser.add_argument('--dropout', default=0.2, type=float)
     parser.add_argument('--epoch', default=12, type=int)
     parser.add_argument('--gpu', default=0, type=int)
     parser.add_argument('--hidden-size', default=100, type=int)
     parser.add_argument('--learning-rate', default=0.5, type=float)
-    parser.add_argument('--print-freq', default=10, type=int)
-    parser.add_argument('--train-batch-size', default=60, type=int)
+    parser.add_argument('--print-freq', default=1000, type=int)
+    parser.add_argument('--train-batch-size', default=50, type=int)
     parser.add_argument('--train-file', default='train-v1.1.json')
     parser.add_argument('--word-dim', default=100, type=int)
     args = parser.parse_args()
@@ -124,7 +126,7 @@ def main():
     setattr(args, 'char_vocab_size', len(data.CHAR.vocab))
     setattr(args, 'word_vocab_size', len(data.WORD.vocab))
     setattr(args, 'dataset_file', f'.data/squad/{args.dev_file}')
-    setattr(args, 'prediction_file', 'prediction.out')
+    setattr(args, 'prediction_file', f'prediction{args.gpu}.out')
     setattr(args, 'model_time', strftime('%H:%M:%S', gmtime()))
     print('data loading complete!')
 
