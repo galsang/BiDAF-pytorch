151c91d02d17374d92e697d44587149f19842da0
galsang <taeuk@europa.snu.ac.kr>
2019-04-30 09:53:11 +0900

    is_target=False argument added to self.RAW

--- model/data.py
+++ model/data.py
@@ -25,7 +25,7 @@ class SQuAD():
         if not os.path.exists(f'{path}/{args.dev_file}l'):
             self.preprocess_file(f'{path}/{args.dev_file}')
 
-        self.RAW = data.RawField()
+        self.RAW = data.RawField(is_target = False)
         self.CHAR_NESTING = data.Field(batch_first=True, tokenize=list, lower=True)
         self.CHAR = data.NestedField(self.CHAR_NESTING, tokenize=word_tokenize)
         self.WORD = data.Field(batch_first=True, tokenize=word_tokenize, lower=True, include_lengths=True)
